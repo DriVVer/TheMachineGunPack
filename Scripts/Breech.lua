0e5f259af6b9bf72bea1870a17c8df12adc7dbcf
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2022-06-26 15:27:13 -0700

    Breech is working now

--- Scripts/Breech.lua
+++ Scripts/Breech.lua
@@ -1,5 +1,5 @@
 --[[
-    Copyright (c) 2022 Questionable Mark
+	Copyright (c) 2022 Questionable Mark
 ]]
 
 dofile("Databases/BreechDatabase.lua")
@@ -12,22 +12,189 @@ Breech.connectionOutput = sm.interactable.connectionType.none
 Breech.colorNormal    = sm.color.new(0x000080ff)
 Breech.colorHighlight = sm.color.new(0x0000edff)
 
-function Breech:client_onCreate()
+local type_to_func_name =
+{
+	[1] = "anim_setup",
+	[2] = "wait_handler",
+	[3] = "effect_handler",
+	[4] = "delay_setup"
+}
 
+local AnimationUpdateFunctions = {}
+AnimationUpdateFunctions.no_animation = function(self, dt) end
+
+AnimationUpdateFunctions.anim_selector = function(self, dt)
+	self.anim_step = self.anim_step + 1
+	if self.anim_step < self.anim_step_count then
+		self.anim_step_data = self.anim_data[self.anim_step]
+		local func_name = type_to_func_name[self.anim_step_data.type]
+
+		self.anim_func = AnimationUpdateFunctions[func_name]
+		self.anim_func(self, dt)
+	else
+		self.anim_func = AnimationUpdateFunctions.no_animation
+		self.anim_step_data = nil
+		self.anim_step = 0
+	end
+end
+
+AnimationUpdateFunctions.anim_setup = function(self, dt)
+	self.anim_time = self.anim_step_data.time
+
+	self.anim_func = AnimationUpdateFunctions.anim_handler
+	self.anim_func(self, dt)
+end
+
+AnimationUpdateFunctions.anim_handler = function(self, dt)
+	local cur_data = self.anim_step_data
+
+	local c_anim_time = cur_data.time
+
+	local predict_time = (self.anim_time - dt)
+	local anim_prog = (c_anim_time - math.max(predict_time, 0)) / c_anim_time
+	local normalized_val = sm.util.clamp(anim_prog, 0.0, 1.0)
+	local final_value = sm.util.lerp(cur_data.start_val, cur_data.end_val, normalized_val)
+
+	local s_interactable = self.interactable
+	for i, anim_name in pairs(cur_data.anim) do
+		s_interactable:setAnimProgress(anim_name, final_value)
+	end
+
+	if self.anim_time then
+		self.anim_time = predict_time
+
+		if self.anim_time <= 0 then
+			self.anim_time = nil
+
+			self.anim_func = AnimationUpdateFunctions.anim_selector
+			self.anim_func(self, dt)
+		end
+	end
+end
+
+AnimationUpdateFunctions.wait_handler = function(self, dt)
+	if self.anim_wait_time then
+		self.anim_wait_time = self.anim_wait_time - dt
+
+		if self.anim_wait_time <= 0.0 then
+			self.anim_wait_time = nil
+
+			self.anim_func = AnimationUpdateFunctions.anim_selector
+			self.anim_func(self, dt)
+		end
+	end
+end
+
+AnimationUpdateFunctions.effect_handler = function(self, dt)
+	local cur_data = self.anim_step_data
+
+	local s_anim_eff = self.anim_effects
+	for k, eff_name in ipairs(cur_data.effects) do
+		s_anim_eff[eff_name]:start()
+	end
+
+	self.anim_func = AnimationUpdateFunctions.anim_selector
+	self.anim_func(self, dt)
+end
+
+AnimationUpdateFunctions.delay_setup = function(self, dt)
+	self.anim_time = self.anim_step_data.time
+
+	self.anim_func = AnimationUpdateFunctions.delay_handler
+	self.anim_func(self, dt)
+end
+
+AnimationUpdateFunctions.delay_handler = function(self, dt)
+	if self.anim_time then
+		self.anim_time = self.anim_time - dt
+
+		if self.anim_time <= 0 then
+			self.anim_time = nil
+
+			self.anim_func = AnimationUpdateFunctions.anim_selector
+			self.anim_func(self, dt)
+		end
+	end
+end
+
+
+
+function Breech:client_onCreate()
+	local anim_data = mgp_getBreechData(self)
+
+	self.anim_effects = {}
+	for effect_name, data in pairs(anim_data.required_effects) do
+		local cur_eff = sm.effect.createEffect(data.name, self.interactable, data.bone)
+		cur_eff:setOffsetPosition(data.offset)
+
+		self.anim_effects[effect_name] = cur_eff
+	end
+
+	local s_interactable = self.interactable
+	for k, anim_name in ipairs(anim_data.required_animations) do
+		s_interactable:setAnimEnabled(anim_name, true)
+	end
+
+	self.anim_duration = 0.0
+	for k, step_data in ipairs(anim_data.animation) do
+		if step_data.type ~= mgp_anim_enum.wait_timer then
+			self.anim_duration = self.anim_duration + (step_data.time or 0)
+		end
+	end
+
+	self.anim_step = 0
+	self.anim_data = anim_data.animation
+	self.anim_step_count = #anim_data.animation + 1
+	self.anim_func = AnimationUpdateFunctions.no_animation
+
+	if sm.isHost then
+		self.sv_cannon_whitelist = anim_data.whitelist
+	end
+end
+
+function Breech:client_startAnimation(reloadTime)
+	if self.anim_step == 0 then
+		self.anim_func = AnimationUpdateFunctions.anim_selector
+		self.anim_wait_time = reloadTime
+	end
 end
 
 function Breech:client_onUpdate(dt)
-	
-end
-
-function Breech:client_onDestroy()
-
+	self.anim_func(self, dt)
 end
 
 function Breech:server_onCreate()
+	self.interactable.publicData = {}
+end
 
+function Breech:server_checkParent(s_interactable)
+	local cur_parent = s_interactable:getSingleParent()
+	if cur_parent ~= self.sv_saved_parent then
+		self.sv_saved_parent = cur_parent
+
+		if self.sv_saved_parent and self.sv_cannon_whitelist[tostring(cur_parent.shape.uuid)] ~= true then
+			cur_parent:disconnect(s_interactable)
+			self.sv_saved_parent = nil
+		end
+	end	
 end
 
 function Breech:server_onFixedUpdate()
-	
+	local s_interactable = self.interactable
+	local s_pub_data = s_interactable.publicData
+
+	self:server_checkParent(s_interactable)
+
+	if s_pub_data.canShoot then
+		s_pub_data.canShoot = false
+
+		local rld_time = s_pub_data.reloadTime or 40
+		local final_duration = (rld_time / 40) - self.anim_duration
+		if final_duration >= 0 then
+			print(final_duration)
+			self.network:sendToClients("client_startAnimation", final_duration)
+		else
+			print("Reload time should be at least", self.anim_duration, "seconds long!")
+		end
+	end
 end
--- Scripts/Databases/BreechDatabase.lua
+++ Scripts/Databases/BreechDatabase.lua
@@ -1,1 +1,34 @@
-print("breach test")
+mgp_anim_enum =
+{
+	bone_animation = 1,
+	wait_timer     = 2,
+	effect         = 3,
+	delay          = 4
+}
+
+local breech_database =
+{
+	["3d410289-0079-4989-ba21-b211562147d5"] =
+	{
+		required_effects = {
+			--test = { name = "BombSmall", offset = sm.vec3.new(0, 0, 0), bone = nil } --that's how you define an effect that can be referenced by the animation
+		},
+		required_animations = { "Shot", "Reload" },
+		animation = {
+			--{ type = mgp_anim_enum.bone_animation, anim = { "Shot" }, start_val = 0.0, end_val = 1.0, time = 1.0 } --that's how you define your animation
+			--{ type = mgp_anim_enum.wait_timer } --that's how you define a custom wait timer that allows any cannon to specify its reload time
+			--{ type = mgp_anim_enum.effect, effects = { "test" } } --that's how you define effects
+			--{ type = mgp_anim_enum.delay, time = 2.0 } --a simple delay in which you can specify any time you want
+			{ type = mgp_anim_enum.bone_animation, anim = { "Shot" }  , start_val = 0.0, end_val = 1.0, time = 1.0 },
+			{ type = mgp_anim_enum.wait_timer },
+			{ type = mgp_anim_enum.bone_animation, anim = { "Reload" }, start_val = 0.0, end_val = 1.0, time = 5.0 }
+		},
+		whitelist = {
+			["03e1ecbd-17ee-4045-a5d8-366f6e656555"] = true --M1 Abrams Cannon (Cannons Pack)
+		}
+	}
+}
+
+function mgp_getBreechData(self)
+	return breech_database[tostring(self.shape.uuid)]
+end
