971e00211dc8bffaa9cfcd2db3b5659124390923
DriVVer <olik.dudu@gmail.com>
2022-06-28 03:27:33 +0200

    Fixes i guess
    new breach and fixing 37mm one

--- Scripts/Databases/BreechDatabase.lua
+++ Scripts/Databases/BreechDatabase.lua
@@ -26,21 +26,39 @@ local breech_database =
 			{ type = mgp_anim_enum.bone_animation, anim = { "Reload" }, start_val = 0.57, end_val = 1.0, time = 2.0 }
 		}
 	},
+	["54b7c549-4b12-4be1-b73d-4d62db371394"] =
+	{
+		required_effects = {
+			--test = { name = "BombSmall", offset = sm.vec3.new(0, 0, 0), bone = nil } --that's how you define an effect that can be referenced by the animation
+		},
+		required_animations = { "Shot", "Reload" },
+		animation = {
+			--{ type = mgp_anim_enum.bone_animation, anim = { "Shot" }, start_val = 0.0, end_val = 1.0, time = 1.0 } --that's how you define your animation
+			--{ type = mgp_anim_enum.wait_timer } --that's how you define a custom wait timer that allows any cannon to specify its reload time
+			--{ type = mgp_anim_enum.effect, effects = { "test" } } --that's how you define effects
+			--{ type = mgp_anim_enum.delay, time = 2.0 } --a simple delay in which you can specify any time you want
+
+			{ type = mgp_anim_enum.bone_animation, anim = { "Shot" }  , start_val = 0.0, end_val = 1.0, time = 1.0 },
+			{ type = mgp_anim_enum.bone_animation, anim = { "Reload" }, start_val = 0.0, end_val = 0.57, time = 3.5 },
+			{ type = mgp_anim_enum.wait_timer },
+			{ type = mgp_anim_enum.bone_animation, anim = { "Reload" }, start_val = 0.57, end_val = 1.0, time = 2.5 }
+		}
+	},
 	["379449f7-27ca-4aea-b723-f841406bbacc"] =
 	{
 		required_effects = {
 			--test = { name = "BombSmall", offset = sm.vec3.new(0, 0, 0), bone = nil } --that's how you define an effect that can be referenced by the animation
 		},
-		required_animations = { "Shot", "Reload1", "Reload2" },
+		required_animations = { "Shot", "Reload" },
 		animation = {
 			--{ type = mgp_anim_enum.bone_animation, anim = { "Shot" }, start_val = 0.0, end_val = 1.0, time = 1.0 } --that's how you define your animation
 			--{ type = mgp_anim_enum.wait_timer } --that's how you define a custom wait timer that allows any cannon to specify its reload time
 			--{ type = mgp_anim_enum.effect, effects = { "test" } } --that's how you define effects
 			--{ type = mgp_anim_enum.delay, time = 2.0 } --a simple delay in which you can specify any time you want
 			{ type = mgp_anim_enum.bone_animation, anim = { "Shot" }  , start_val = 0.0, end_val = 1.0, time = 1.0 },
-			{ type = mgp_anim_enum.bone_animation, anim = { "Reload1" }, start_val = 0.0, end_val = 1.0, time = 2.0 },
+			{ type = mgp_anim_enum.bone_animation, anim = { "Reload" }, start_val = 0.0, end_val = 0.50, time = 1.3 },
 			{ type = mgp_anim_enum.wait_timer },
-			{ type = mgp_anim_enum.bone_animation, anim = { "Reload2" }, start_val = 0.0, end_val = 1.0, time = 2.0 }
+			{ type = mgp_anim_enum.bone_animation, anim = { "Reload" }, start_val = 0.50, end_val = 1.0, time = 1.2 }
 		
 		}
 	}
