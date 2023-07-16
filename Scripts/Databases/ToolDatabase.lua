607157f22adfd9f8fca02afcd7295b9155ea21d4
DriVVer <olik.dudu@gmail.com>
2023-07-16 23:39:38 +0200

    Pistol stuff

--- Scripts/Databases/ToolDatabase.lua
+++ Scripts/Databases/ToolDatabase.lua
@@ -123,6 +123,90 @@ local mgp_tool_database =
 		}
 	},
 
+	m1911 =
+	{
+		required_effects = {
+			shoot_tp = "Muzzle_Flash_SmallCal_tp",
+			shoot_fp = "Muzzle_Flash_SmallCal_fp",
+			reloadTG = "TommyReload",
+			reloadETG = "TommyEReload"
+		},
+		on_unequip_action = {
+			stop_effects = { "reloadTG", "reloadETG" }
+		},
+		animation = {
+			shoot =
+			{
+				[1] = {
+					{
+						type = mgp_tool_anim_enum.effect,
+						bone = "pejnt_barrel",
+						name_tp = "shoot_tp",
+						name_fp = "shoot_fp",
+						tp_offset = sm.vec3.new(0, 0.5, 0),
+						fp_offset = sm.vec3.new(0.0, 0.07, 0),
+						apply_velocity = false
+					},
+					{
+						type = mgp_tool_anim_enum.particle,
+						fp_offset = sm.vec3.new(0, -0.07, 0),
+						tp_offset = sm.vec3.new(0, 0, 0),
+						name_tp = "TommyShell",
+						name_fp = "TommyShellFP",
+						bone_name = "jnt_slide"
+					},
+					{
+						type = mgp_tool_anim_enum.bone_animation,
+						fp_anim = { { name = "M1911_anims", start_val = 0.0, end_val = 1.0 } },
+						tp_anim = { { name = "M1911_anims", start_val = 0.0, end_val = 1.0 } },
+						time = 1.0
+					}
+				}
+			},
+			reload =
+			{
+				[1] = {
+					{
+						type = mgp_tool_anim_enum.effect,
+						bone = "pejnt_barrel",
+						name_tp = "reloadTG",
+						name_fp = "reloadTG",
+						tp_offset = sm.vec3.new(0, 0, 0),
+						fp_offset = sm.vec3.new(0, 0, 0),
+						apply_velocity = false,
+
+					},
+					{
+						type = mgp_tool_anim_enum.bone_animation,
+						fp_anim = { { name = "M1911_anims", start_val = 0.0, end_val = 5.0 } },
+						tp_anim = { { name = "M1911_anims", start_val = 0.0, end_val = 5.0 } },
+						time = 5.0
+					}
+				}
+			},
+			reload_empty =
+			{
+				[1] = { --first animation track
+					{
+						type = mgp_tool_anim_enum.effect,
+						bone = "pejnt_barrel",
+						name_tp = "reloadETG",
+						name_fp = "reloadETG",
+						tp_offset = sm.vec3.new(0, 0, 0),
+						fp_offset = sm.vec3.new(0, 0, 0),
+						apply_velocity = false
+					},
+					{
+						type = mgp_tool_anim_enum.bone_animation,
+						fp_anim = { { name = "M1911_anims", start_val = 0.0, end_val = 5.0 } },
+						tp_anim = { { name = "M1911_anims", start_val = 0.0, end_val = 5.0 } },
+						time = 5.0
+					}
+				}
+			}
+		}
+	},
+
 	Mp40 =
 	{
 		required_effects = {
